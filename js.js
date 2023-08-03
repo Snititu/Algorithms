6a0845038ff7b5e2c1dacc5ad8b2294f0f88db3b
Snit <snitxegay2@gmail.com>
2023-08-03 15:34:03 +0200

    this my first alg. ass

---
+++ js.js
@@ -0,0 +1,71 @@
+/* exercise 1
+
+for (let i = 1; i <= 135; i++) {
+  console.log(i);
+}*/
+
+/* exercise 2
+
+for (let i=1 ; i<=135 ; i+=2){
+  console.log(i)
+}*/
+
+/*exercise 3
+
+let total = 0
+for (let i=1 ; i<=135 ; i++){
+ total = total + i
+console.log(`number is ${i} sum ${total}`)
+}
+*/
+
+/*exercise 4
+
+const x = [1,4,2,12]
+for (let i=0 ; i<x.length ; i++){
+  console.log(x[i])
+} */
+
+/*exercise 5
+const arr = [4, 6, 2, -1, 10];
+let max = arr[0];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > max) {
+    max = arr[i];
+    
+  }
+}
+console.log(max);*/
+
+// exercise 6
+
+/*const  x = [2,1,3]
+let sum = 0 ;
+
+for (let i=0 ; i<x.length ; i++){
+
+  sum += x[i]
+}
+console.log(sum/ x.length) */
+
+/*
+exercise 7
+ const x= [2,-1,4,-3]
+
+ for (let i= 0 ; i<x.length ; i++){
+    if ( x[i]<0){
+      x[i]=0 
+    }
+ }
+ console.log( x)*/
+
+//exercise 8
+
+/* const x = [1, -4, 0, -1];
+for (let i = 0; i < x.length; i++) {
+  if (x[i] < 0) {
+    x[i] = "Snit";
+  }
+}
+console.log(x);
+*/
